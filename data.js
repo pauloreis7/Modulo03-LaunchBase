32d53ca9905f27bc5050b356188b6087a0b538fc
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-04-30 22:05:39 -0300

    Separação de dados entre backend e frontend
    Exercendo mais a ideia de backend e frontend, separando os dados para o backend por engine e exercitando conceitos

---
+++ data.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "formulário animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free",
+        featured: true
+    },
+
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o css grid na prática | Mayk Brito",
+        duration: "36 min",
+        price: "Free"
+    },
+
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: Higher Order Functions | Mayk Brito",
+        duration: "54 min",
+        price: "Free"
+    },
+
+    {
+        id: "H91DhKPjhPk",
+        title: "Responsividade na Prática | Masterclass #08 | Diego Fernandes",
+        duration: "1h 30 min",
+        price: "Free",
+        featured: true
+
+    },
+
+    {
+        id: "P65RJTTqkN4",
+        title: "Formulários no ReactJS & React Native com Unform | MasterClass #06 | Diego Fernandes",
+        duration: "47 min",
+        price: "Free"
+    },
+
+    {
+        id: "dlCUvYOlnDo",
+        title: "Usando Serverless, Jest e Node.js para corrigir desafios | Behind the Code #13 | Diego Fernandes",
+        duration: "55 min",
+        price: "Free",
+        featured: true
+    }
+
+
+]
--- server.js
+++ server.js
@@ -2,21 +2,31 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape:false,
+    noChace:true
 })
 
 server.get("/", function (req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "foto_user.png",
+        name: "Paulo Reis",
+        rule: "Aluno - Rocketseat",
+        description: 'Amante da programação e apaixonado pelas melhores tecnologias do mercado. Focado em se tornar um desenvovedor sênior full-stack com JavaScript e as tecnologias omnistack. Aluno da <a href="http://Rocketseat.com.br" target="_blank" >Rocketseat</a>'
+    }
+
+    return res.render("about", {about})
 })
 
 server.get("/portifolio", function (req, res) {
-    return res.render("portifolio")
+    return res.render("portifolio", {itens:videos})
 })
 
 server.listen(5000, function () {
